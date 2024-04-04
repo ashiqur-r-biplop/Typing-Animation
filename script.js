058883225188d0700dc08a9fbaaf696862be7dd3
ashiqur-r-biplop <biplops22@gmail.com>
2024-04-04 15:30:57 +0600

    added some code

--- script.js
+++ script.js
@@ -1,32 +1,44 @@
-function startTypingAnimation(text, containerId, speed = 100, delay = 1000, callback) {
-    let index = 0;
-    const container = document.getElementById(containerId);
-    
-    function render() {
-      if (index < text.length) {
-        container.textContent += text.charAt(index);
-        index++;
-        setTimeout(render, speed);
-      } else {
-        setTimeout(callback, delay);
-      }
+function startTypingAnimation(
+  text,
+  containerId,
+  speed = 100,
+  delay = 1000,
+  callback
+) {
+  let index = 0;
+  const container = document.getElementById(containerId);
+  container.textContent = "I am ";
+  function render() {
+    if (index < text.length) {
+      container.textContent += text.charAt(index);
+
+      index++;
+      setTimeout(render, speed);
+    } else {
+      setTimeout(callback, delay);
     }
-
-    render();
   }
+  render();
+}
 
-  // Usage
-  const textToType1 = "Hello, this is the first typing animation! ";
-  const textToType2 = "And this is the second typing animation! ";
-  
-  function startAnimations() {
-    startTypingAnimation(textToType1, 'text-container1', 100, 2000, function() {
-      document.getElementById('text-container1').textContent = '';
-      startTypingAnimation(textToType2, 'text-container2', 100, 2000, function() {
-        document.getElementById('text-container2').textContent = '';
+// Usage
+const textToType1 = "Ashiqur Rahman Biplop. ";
+const textToType2 = "Front End web developer.";
+
+function startAnimations() {
+  startTypingAnimation(textToType1, "text-container1", 100, 2000, function () {
+    document.getElementById("text-container1").textContent = "";
+    startTypingAnimation(
+      textToType2,
+      "text-container2",
+      100,
+      2000,
+      function () {
+        document.getElementById("text-container2").textContent = "";
         startAnimations();
-      });
-    });
-  }
+      }
+    );
+  });
+}
 
-  startAnimations();
+startAnimations();
